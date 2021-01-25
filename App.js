7be00a96c86f241dc60b17c7be771829d0224678
vladkovalevsky <kovalevskyi.vk@gmail.com>
2021-01-25 22:00:25 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    marginTop: 20,
   },
+  content: {
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 12,
+  }
 });
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+
+    this.keyExtractor = this.keyExtractor.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ data: json });
+      })
+      .catch((error) => console.error(`Oops! ... something went wrong`))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  keyExtractor(item) {
+    return item.id;
+  }
+
+  renderItem({ item }) {
+    const { data } = this.state;
+
+      return (
+        <View style={styles.content}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.body}>{item.body}</Text>
+        </View>
+      );
+    }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+        <View style={styles.container}>
+          {isLoading ? <ActivityIndicator/> : (
+            <FlatList
+              data={data}
+              renderItem={this.renderItem}
+              keyExtractor={this.keyExtractor}
+            />
+          )}
+        </View>
+    );
+  }
+}
